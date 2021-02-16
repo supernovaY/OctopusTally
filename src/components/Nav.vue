ccdac3a9a88d5abb60609917fb26d6c6d5e5e721
Supernova <supernova_gu@outlook.com>
2021-02-16 20:33:48 +0800

    创建了 <Icon />组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
-  <router-link to="/money">记账</router-link>
+  <div class="nav">
+  <router-link to="/money">
+    <icon name="money"></icon>
+    记账</router-link>
   |
-  <router-link to="/labels">标签</router-link>
+  <router-link to="/labels">
+    <icon name="label"></icon>
+    标签</router-link>
   |
-  <router-link to="/statistics">统计</router-link>
+  <router-link to="/statistics">
+    <icon name="statistics"></icon>
+    统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
+  // import x from '@/assets/icons/label.svg'
+  // import y from '@/assets/icons/money.svg'
 export default {
   name: "Nav"
 }
 </script>
 
 <style scoped lang="scss">
-
+  .nav {border: 1px solid red}
 </style>
