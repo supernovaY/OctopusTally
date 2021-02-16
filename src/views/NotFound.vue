fc8ff99318523bdb741ba7a97888cb619d88c166
Supernova <supernova_gu@outlook.com>
2021-02-16 19:03:54 +0800

    创建了 <Layout />组件

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <div>当前路径不存在，请检查路径</div>
+  <div>
+    <router-link to="/">返回首页</router-link>
+<!--    <a href="#/">返回首页</a>-->
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+name: "NotFound"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
