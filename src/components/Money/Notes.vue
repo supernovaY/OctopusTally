393a09348940018c069b5face3a3cd3f46883a0a
Supernova <supernova_gu@outlook.com>
2021-02-18 16:01:15 +0800

    监听四个组件的值变化，并放到record里

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -12,11 +12,15 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import {Component} from 'vue-property-decorator'
+import {Component, Watch} from 'vue-property-decorator';
 
 @Component
 export default class NumberPad extends Vue {
   value = ''
+  @Watch('value')
+  onValueChanged(value: string){
+    this.$emit('update:value', value)
+  }
 }
 </script>
 
